67799d1a97d3ea8fcc003d0e07f4d5f0e5647e8d
Arkady Lacquered <a.novokhatskii@g.nsu.ru>
2021-09-14 20:55:25 +0700

    Add first lesson result

---
+++ src/test/java/com/github/lacquerednocap/SpringSimpleWebsiteLetsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lacquerednocap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleWebsiteLetsCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

fbd452b11914066e6197000fbc17d262aab3b3ee
Arkady Lacquered <a.novokhatskii@g.nsu.ru>
2021-09-21 18:12:10 +0700

    Lesson 2 is done!

--- src/main/java/com/github/lacquerednocap/GreetingController.java
+++ src/main/java/com/github/lacquerednocap/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Map;
+import java.util.*;
 
 @Controller
 public class GreetingController {
@@ -26,7 +26,6 @@ public class GreetingController {
     @GetMapping
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
-
         model.put("messages", messages);
 
         return "main";
@@ -35,11 +34,23 @@ public class GreetingController {
     @PostMapping
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag);
-
         messageRepo.save(message);
-
         Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
 
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public  String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        }
+        else {
+            messages = messageRepo.findAll();
+        }
         model.put("messages", messages);
 
         return "main";
--- src/main/java/com/github/lacquerednocap/repos/MessageRepo.java
+++ src/main/java/com/github/lacquerednocap/repos/MessageRepo.java
@@ -3,5 +3,9 @@ package com.github.lacquerednocap.repos;
 import com.github.lacquerednocap.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
 }
